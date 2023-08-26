e9f0bbc92f1da297c8928d06847e5dc0b9d06603
Admin <hodondoc001@gmail.com>
2023-08-26 20:49:27 +0700

    init project

---
+++ src/test/java/vti/edu/vtiacademy/vtiassignment5/VtiAssignment5ApplicationTests.java
@@ -0,0 +1,13 @@
+package vti.edu.vtiacademy.vtiassignment5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VtiAssignment5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
